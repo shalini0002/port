2071c3df359e120bd760c4c30d27d53d5de77c12
Shalini <shalinisharmar60@gmail.com>
2023-08-17 22:55:58 +0530

    project changes

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
 
+window.addEventListener('scroll', () => {
+    // console.log("scrlooed");
+    navMenu.classList.remove("active");
+ })
+
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+     
 }))
+
+// const scroll = document.querySelector(".nav-menu");
+// console.log(scroll);
